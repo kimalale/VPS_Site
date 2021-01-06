5dbf64f2dedd1342fe1522cdcff0189e557be4ea
kimalale <75997122+kimalale@users.noreply.github.com>
2021-01-06 02:13:56 +0200

    Update server.php

--- Version 3.0/server.php
+++ Version 3.0/server.php
@@ -1,14 +1,12 @@
 <?php
-$site_name        = "LadyClare";
+$site_name        = "Exxotic";
 $site_description = "Provider of Quality SSH, SSL, VPN Accounts";
 $site_template    = "lumen";
 
 $daily_limit_user = "50";
 
 $server_lists_array=array(
-	1=>array(1=>"Singapore AWS","13.251.88.112","LadyClare","5"),
-	2=>array(1=>"Germany AWS","18.184.9.161","LadyClare","5"),
-	3=>array(1=>"London AWS","18.130.94.241","LadyClare","5"),
+	   1=>array(1=>"London Server","138.68.58.37","Exxotic","5"),
 
 );
 
